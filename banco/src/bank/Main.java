5e191d44981f3b1e4f356428f18eb9ffe39efd5e
Vitor Lemos <164427742+Vitul1906@users.noreply.github.com>
2025-02-18 03:04:42 +0000

    teste_02

--- banco/src/bank/Main.java
+++ banco/src/bank/Main.java
@@ -1,10 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Classe principal que demonstra a criação e manipulação de uma lista de clientes.
+ * 
+ * O programa cria uma lista de clientes com nomes e idades variáveis, tratando exceções caso a idade seja inválida.
+ * Em seguida, imprime a lista de clientes, reduz a idade de cada um em um ano e exibe a lista modificada.
+ * 
+ * Dependências:
+ * - Cliente: Classe que representa um cliente com nome e idade.
+ * - IllegalAgeException: Exceção personalizada para idades inválidas, criação de classe pedida no exercício.
+ */
+
 public class Main {
   public static void main(String[] args) {
     List<Cliente> clientes = new ArrayList<>();
     List<Cliente> clientes2 = new ArrayList<>();
+
+// Criando 20 clientes com nomes diferentes e idades variáveis
 
     for(int i=0; i<20; i++) {
       try {
